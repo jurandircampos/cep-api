2c07f591d578eb3d5e4977999b834107b81d4564
jurandircampos <jurandir.git@gmail.com>
2025-10-14 08:40:02 -0300

    Criação do projeto cep-api

---
+++ src/test/java/com/nava/cep/CepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nava.cep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
